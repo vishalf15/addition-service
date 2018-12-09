355fe0b179304de69cb0490470710923254d52f7
vishalf15 <vishalf15@gmail.com>
2018-12-09 13:52:25 +0530

    RnD to resolve CodeDeploy failure. 1

--- scripts/change-to-exe-prvillages.sh
+++ scripts/change-to-exe-prvillages.sh
@@ -1,3 +1,6 @@
-#!/usr/bin bash
+#!/bin/bash
 echo 'Upgrading privilages to executable'
-chmod 755 run-addition-service.sh
+chmod 777 /home/ec2-user/apps/addition-service/scripts/change-to-exe-prvillages.sh
+chmod 777 /home/ec2-user/apps/addition-service/scripts/run-addition-service.sh
+chmod 777 scripts/change-to-exe-prvillages.sh
+chmod 777 scripts/run-addition-service.sh
--- scripts/run-addition-service.sh
+++ scripts/run-addition-service.sh
@@ -1,3 +1,3 @@
-#!/usr/bin bash
+#!/bin/bash
 echo 'Starting Spring Boot app'
 cd '/home/ec2-user/apps/addition-service'
