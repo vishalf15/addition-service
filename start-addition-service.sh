adf9cdf7d5addb3d1813bfe1a528dee3f12f026b
vishalf15 <vishalf15@gmail.com>
2018-12-09 20:48:56 +0530

    RnD to resolve CodeDeploy failure. 13

--- start-addition-service.sh
+++ start-addition-service.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-echo 'Starting Spring Boot app'
-cd '/home/ec2-user/apps/addition-service'
 echo 'current path = '
 pwd
-echo 'Maven install started '
-mvn install
+echo 'List of files = '
+ls
+echo 'Starting Spring Boot app'
+cd '/home/ec2-user/apps/addition-service'
+echo 'new current path = '
+pwd
 echo 'List of files = '
 ls
 java -jar addition-service-1.0.0.jar
