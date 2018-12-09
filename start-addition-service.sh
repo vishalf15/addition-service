85f87fef0bb1b4248b6c9fbc9bdd9df502057493
vishalf15 <vishalf15@gmail.com>
2018-12-10 00:02:41 +0530

    removed 'mvn install' command

--- start-addition-service.sh
+++ start-addition-service.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 echo 'Starting Spring Boot app'
 cd '/home/ec2-user/apps/addition-service'
-mvn install
-echo 'new current path = '
-pwd
-echo 'List of files = '
-ls
 java -jar addition-service-1.0.0.jar
