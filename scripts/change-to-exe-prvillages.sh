677c0b291d57bfa0ec3b4ac124f51170f1b68f19
vishalf15 <vishalf15@gmail.com>
2018-12-09 14:55:05 +0530

    RnD to resolve CodeDeploy failure. 3

--- scripts/change-to-exe-prvillages.sh
+++ scripts/change-to-exe-prvillages.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 echo 'Upgrading privilages to executable'
-chmod 777 /home/ec2-user/apps/addition-service/scripts/change-to-exe-prvillages.sh
-chmod 777 /home/ec2-user/apps/addition-service/scripts/run-addition-service.sh
-chmod 777 scripts/change-to-exe-prvillages.sh
-chmod 777 scripts/run-addition-service.sh
