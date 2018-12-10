93375dabef64cb9dedf6a6553b70ce7de73992ec
vishalf15 <vishalf15@gmail.com>
2018-12-10 23:52:54 +0530

    Copying executable jar from s3 bucket to temporary location

--- start-addition-service.sh
+++ start-addition-service.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo 'Starting Spring Boot app'
 cd '/home/ec2-user/apps/addition-service'
+aws s3 cp s3://awscodepipeline-demobucket-addition-service/addition-service /home/ec2-user/apps/addition-service --region=ap-south-1
 java -jar addition-service-1.0.0.jar
