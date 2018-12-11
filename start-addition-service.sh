02f32fe5aa47a6785219835491be1b9e3e8a915b
vishalf15 <vishalf15@gmail.com>
2018-12-11 23:48:23 +0530

    s3 copy command changed

--- start-addition-service.sh
+++ start-addition-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'Starting Spring Boot app'
 cd '/home/ec2-user/apps/addition-service'
-aws s3 cp s3://awscodepipeline-demobucket-addition-service/addition-service /home/ec2-user/apps/addition-service --region=ap-south-1
+aws s3 cp s3://awscodepipeline-demobucket-addition-service/addition-service-1.0.0.jar /home/ec2-user/apps/addition-service --region=ap-south-1
 java -jar addition-service-1.0.0.jar
